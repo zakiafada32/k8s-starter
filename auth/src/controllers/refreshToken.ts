2536ba6136258501808aefd33d2ef75b46986146
zakiafada32 <afadazaki32@gmail.com>
2021-05-30 12:05:51 +0700

    refresh token

--- auth/src/controllers/refreshToken.ts
+++ auth/src/controllers/refreshToken.ts
@@ -1,12 +1,62 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import RefreshTokenModel from '../models/token';
+import UserModel from '../models/user';
 
-export const refreshToken = async (req: Request, res: Response) => {
+interface UserPayload {
+  id: string;
+  email: string;
+  name: string;
+  role: string;
+}
+interface Body {
+  token: string;
+}
+interface CustomRequest<T> extends Request {
+  body: T;
+}
+
+export const refreshToken = async (req: CustomRequest<Body>, res: Response) => {
   try {
-    console.log('req.body: ', req.body);
-    res.status(200).send({ data: 'refresh token' });
+    const { token } = req.body;
+    const refreshToken = await RefreshTokenModel.findOne({
+      refreshToken: token,
+    });
+    if (!refreshToken) {
+      throw new Error('Not Authorized');
+    }
+
+    const payload = jwt.verify(
+      token,
+      process.env.JWT_REFRESH_KEY!
+    ) as UserPayload;
+    const user = await UserModel.findById(payload.id);
+    if (!user) {
+      throw new Error('Not Authorized');
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
+      process.env.JWT_KEY!,
+      { expiresIn: '5m' }
+    );
+
+    res
+      .status(200)
+      .send({
+        status: 'success',
+        message: 'access token created',
+        accessToken,
+      });
   } catch (err) {
     res.status(400).send({
-      status: 'refresh token not success',
+      status: 'Unsuccessful',
+      message: err.message,
     });
   }
 };
